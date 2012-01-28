3e440908124c3579a48e0f46b841f38c3fc5368d
Thilo Utke <thilo@upstre.am>
2012-01-28 22:35:25 +0100

    new slideshow and first elements

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(function() {
- 
+  var $scrollable = $("#makeMeScrollable");
+  $scrollable.smoothDivScroll({});
+  $scrollable.find(".scrollingHotSpot").bind('mouseover', function(){
+    $scrollable.smoothDivScroll("showHotSpotBackgrounds");
+  });
+  
+  $scrollable.find(".scrollingHotSpot").bind('mouseleave', function(){
+    $scrollable.smoothDivScroll("hideHotSpotBackgrounds");
+  });
+  
+  
 });
